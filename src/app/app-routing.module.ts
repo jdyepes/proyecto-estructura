c1f3d39c32f786ba1ebea07fb8d3ed9afbcd89fb
jdyepes <jesusyepes.1205@gmail.com>
2022-02-08 23:40:42 -0400

    Creacion de estructura proyecto prototipo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'demo', component: DemoComponent },
